93f27a8e65c85775991535811c5d603f5d22404f
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-03-23 15:15:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+//function expression called divine
+
+const divide = function() {
+    return 2000/100;
+}
+//arrow function called square
+const square = param => param * param;
+
+//arrow function called add
+const add = (param1, param2) => param1 + param2;
